a1b2175336afc2146b14236fe6ececc020580957
Carl Barringer <cbarringer1@gmail.com>
2019-10-31 12:56:07 -0500

    initial commit

---
+++ Models/Instructor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercisesAPI.Models
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string SlackHandle { get; set; }
+        public int CohortId { get; set; }
+        public Cohort Cohort {get; set; }
+        public string Specialty { get; set; }
+        public void AssignExercise ( Student student, Exercise exercise )
+        {
+            student.Exercises.Add(exercise);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercisesAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string SlackHandle { get; set; }
+        public int CohortId { get; set; }
+        public Cohort Cohort { get; set; }
+
+        public List<Exercise> Exercises = new List<Exercise>();
+    }
+}
